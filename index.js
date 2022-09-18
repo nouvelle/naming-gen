0c18d42e8ad329e525eb415bb0a7c05efccb8116
nouvelle <nouvellenuts@gmail.com>
2022-09-18 23:19:10 +0900

    feat: add main feature

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const axios = require('axios')
+const c = require('ansi-colors')
+const { Input } = require('enquirer')
+
+welcomeMsg()
+checkApiKey()
+inputWord()
+
+function welcomeMsg () {
+  console.log(c.yellow("\n  Welcome to FIND NAMING!!🎉 \n  Let's find good variable name 😁 \n"))
+}
+
+function checkApiKey () {
+  if (!process.env.CODIC_ACCESS_TOKEN) {
+    console.log(c.red('👀 Please set your API key.'))
+  }
+}
+
+function inputWord () {
+  const prompt = new Input({
+    message: 'ネーミングを作成してほしい言葉を日本語で入力してください 👉'
+  })
+
+  prompt.run()
+    .then(async (answer) => {
+      const result = await seatchName(answer)
+      console.log('👉 ' + c.cyan(result.translated_text) + '\n')
+    })
+    .catch(err => console.error(err))
+}
+
+async function seatchName (word, casing = 'camel') {
+  const url = `https://api.codic.jp/v1/engine/translate.json?text=${word}&casing=${casing}`
+  const encodedUrl = encodeURI(url)
+  const token = process.env.CODIC_ACCESS_TOKEN
+
+  const response = await axios.get(encodedUrl, { headers: { Authorization: `Bearer ${token}` } })
+    .catch(err => console.error(err))
+  return response.data[0]
+}
